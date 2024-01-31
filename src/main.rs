23c44916cfe48e0bb6f4f77fe4caecd7e6cb869a
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2024-01-31 07:57:29 -0500

    finish telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use z2p::{configuration::get_configuration, startup::run};
+use z2p::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind port 8000");
     run(listener, connection_pool)?.await
